214d91f45d33ee8d1cab1aa5112e573bba8e5810
Lino Jorge <109708821+Linolonil@users.noreply.github.com>
2024-03-29 17:32:44 -0400

    corrigindo background 3

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -1,103 +1,103 @@
 import style from './cards.module.css'
 
-export default function Cards (){
 
+export default function Cards (){
   const integrantesData = [
       {
         "id": 1,
         "nome": "Limoeiro",
         "funcao": "ALL LANES",
         "elo": "esmeralda",
-        "champion": "../../../public/champion/maokai.jpg"  ,
-        "imagem": "../../../public/criminosos/Eu.png"
+        "champion": "../../src/assets/champion/maokai.jpg"  ,
+        "imagem": "../../src/assets/criminosos/Eu.png"
       },
       {
         "id": 2,
         "nome": "Aiox",
         "funcao": "MID/ADC",
         "elo": "diamante",
-        "champion": "../../../public/champion/anivia.jpg"  ,
-        "imagem": "../../../public/criminosos/Aiox.jpg"
+        "champion": "../../src/assets/champion/anivia.jpg"  ,
+        "imagem": "../../src/assets/criminosos/Aiox.jpg"
       },
       {
         "id": 3,
         "nome": "Pira",
         "funcao": "ALL LANES",
         "elo": "ouro",
-        "champion": "../../../public/champion/amumu.jpg"  ,
-        "imagem": "../../../public/criminosos/pira.jpg"
+        "champion": "../../src/assets/champion/amumu.jpg",
+        "imagem": "../../src/assets/criminosos/pira.jpg"
       },
       {
         "id": 4,
         "nome": "Thales",
         "funcao": "ADC/SUP",
         "elo": "ouro",
-        "champion": "../../../public/champion/lucian.jpg"  ,
-        "imagem": "../../../public/criminosos/Eu.png"
+        "champion": "../../src/assets/champion/lucian.jpg"  ,
+        "imagem": "../../src/assets/criminosos/Eu.png"
       },
       {
         "id": 5,
         "nome": "Lari",
         "funcao": "SUP",
         "elo": "ouro",
-        "champion": "../../../public/champion/galio.jpg"  ,
-        "imagem": "../../../public/criminosos/Eu.png"
+        "champion": "../../src/assets/champion/galio.jpg"  ,
+        "imagem": "../../src/assets/criminosos/Eu.png"
       },
       {
         "id": 6,
         "nome": "Marconha",
         "funcao": "ADC/JG",
         "elo": "esmeralda",
-        "champion": "../../../public/champion/kindred.jpg"  ,
-        "imagem": "../../../public/criminosos/marconha.jpg"
+        "champion": "../../src/assets/champion/kindred.jpg"  ,
+        "imagem": "../../src/assets/criminosos/marconha.jpg"
       },
       {
         "id": 7,
         "nome": "Brayan",
         "funcao": "JG",
         "elo": "ouro",
-        "champion": "../../../public/champion/rammus.jpg"  ,
-        "imagem": "../../../public/criminosos/Braia.jpg"
+        "champion": "../../src/assets/champion/rammus.jpg"  ,
+        "imagem": "../../src/assets/criminosos/Braia.jpg"
       },
       {
         "id": 8,
         "nome": "Kraudin",
         "funcao": "JG",
         "elo": "esmeralda",
-        "champion": "../../../public/champion/nunu.jpg"  ,
-        "imagem": "../../../public/criminosos/Eu.png"
+        "champion": "../../src/assets/champion/nunu.jpg"  ,
+        "imagem": "../../src/assets/criminosos/Eu.png"
       },
       {
         "id": 9,
         "nome": "Nakawak",
         "funcao": "ADC",
         "elo": "platina",
-        "champion": "../../../public/champion/rato.jpg"  ,
-        "imagem": "../../../public/criminosos/Eu.png"
+        "champion": "../../src/assets/champion/rato.jpg"  ,
+        "imagem": "../../src/assets/criminosos/Eu.png"
       },
       {
         "id":10,
         "nome": "Gar",
         "funcao" : "TOP/ADC",
         "elo": "platina",
-        "champion": "../../../public/champion/trundle.jpg"  ,
-        "imagem": "../../../public/criminosos/gar.jpg"
+        "champion": "../../src/assets/champion/trundle.jpg"  ,
+        "imagem": "../../src/assets/criminosos/gar.jpg"
       },
       {
         "id":11,
         "nome": "Natália",
         "funcao" : "ADC/SUP",
         "elo": "platina",
-        "champion": "../../../public/champion/xayah.jpg"  ,
-        "imagem": "../../../public/criminosos/natalia.jpg"
+        "champion": "../../src/assets/champion/xayah.jpg"  ,
+        "imagem": "../../src/assets/criminosos/natalia.jpg"
       },
       {
         "id":12,
         "nome": "Willzada",
         "funcao" : "SUP",
         "elo": "ouro",
-        "champion": "../../../public/champion/zyra.jpg"  ,
-        "imagem": "../../../public/criminosos/will.jpg"
+        "champion": "../../src/assets/champion/zyra.jpg"  ,
+        "imagem": "../../src/assets/criminosos/will.jpg"
       }
     ];
   
